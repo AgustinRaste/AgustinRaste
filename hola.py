87e5936c1af59f03545943543bf0467f1c316407
Agustin Rastellini <agustinrastellini@Air-de-Agustin.fibertel.com.ar>
2021-08-11 20:18:27 -0300

    Commit de prueba para controlar versiones desde MacBook

---
+++ hola.py
@@ -0,0 +1,15 @@
+print('Esto es un programa de prueba para iniciar un repo desde MacBook')
+
+op = int(input('Ingrese una opcion(1, 2 o 3) con -1 corta: '))
+while True:
+    if op == 1:
+        print('Opcion 1')
+    if op == 2:
+        print('Opcion 2')
+    if op == 3:
+        print('Fiuuumba')
+    if op == -1:
+        print('SALIENDO...')
+        break
+    op = int(input('Ingrese otra opcion: '))
+
